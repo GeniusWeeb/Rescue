dcb1c0c4c73eba87bdd064891431f3f0393a8528
moAnmol <anmol@allstargames.io>
2023-06-11 22:55:51 +0100

    #jumping changes and skin width

--- Assets/Prototype/Scripts/GroundCheckerCast.cs
+++ Assets/Prototype/Scripts/GroundCheckerCast.cs
@@ -1,6 +1,8 @@
 
 using Rescue.CharacterFSM;
 using UnityEngine;
+using UnityEditor;
+
 
 
 public class GroundCheckerCast : MonoBehaviour
@@ -13,16 +15,19 @@ public class GroundCheckerCast : MonoBehaviour
     [SerializeField] private float maxDistance ;
 
     [SerializeField] private bool isGrounded;
+    [SerializeField]private Vector3 castSize ; 
     
     private void Awake()
     {
        //  collider = this.GetComponent<Collider>();
+       
+       
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-     detect =    Physics.BoxCast(this.transform.parent.position, this.transform.parent.localScale/2,
-         -transform.up,  out hit, transform.rotation , maxDistance     ,layerGround);
+     detect =    Physics.BoxCast(this.transform.position, castSize/2,
+         Vector3.down,  out hit, transform.rotation ,maxDistance,layerGround);
      if (detect)
      {
          isGrounded = true;
@@ -40,14 +45,15 @@ public class GroundCheckerCast : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;  
-        Gizmos.DrawRay(this.transform.position,-transform.up );
-        Gizmos.DrawWireCube(this.transform.parent.position * maxDistance , this.transform .parent.localScale/2);
+        Gizmos.DrawRay(this.transform.position ,Vector3.down * maxDistance );
+        Gizmos.DrawWireCube(this.transform.position ,castSize);
      
          
         if (detect)
         {
             Gizmos.color = Color.green;
-            Gizmos.DrawWireCube( this.transform.parent.position * maxDistance , this.transform.parent.localScale/2);
+            Gizmos.DrawWireCube( this.transform.position,castSize/2);
+            Gizmos.DrawRay(this.transform.position,Vector3.down * maxDistance );
         }
     }
 }
